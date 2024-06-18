d18b86286194dc1adc4b86c643347bf7c367aff5
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-06-18 12:03:47 +0800

    table saijruulsan

---
+++ src/components/segments/table.tsx
@@ -0,0 +1,103 @@
+import {
+  EuiBasicTableColumn,
+  EuiTableFieldDataColumnType,
+  EuiBasicTable,
+  formatDate,
+} from "@elastic/eui";
+import moment from "moment";
+import router from "next/router";
+import useGetSegments, { Segment } from "../../hooks/useGetSegments";
+
+const pathPrefix = process.env.PATH_PREFIX;
+
+const SegmentsTable = () => {
+  const { data, isLoading } = useGetSegments<Segment[]>();
+
+  const columns: Array<EuiBasicTableColumn<Segment>> = [
+    {
+      field: "id",
+      name: "ID",
+      "data-test-subj": "idCell",
+      mobileOptions: {
+        render: (segment: Segment) => <>{segment.id}</>,
+        enlarge: true,
+      },
+    },
+    {
+      field: "name",
+      name: "Name",
+      "data-test-subj": "nameCell",
+      mobileOptions: {
+        render: (segment: Segment) => <>{segment.name}</>,
+        enlarge: true,
+      },
+    },
+    {
+      field: "description",
+      name: "Description",
+      "data-test-subj": "descriptionCell",
+      mobileOptions: {
+        render: (segment: Segment) => <>{segment.description}</>,
+        enlarge: true,
+      },
+    },
+    {
+      field: "type",
+      name: "Type",
+      "data-test-subj": "typeCell",
+      mobileOptions: {
+        render: (segment: Segment) => <>{segment.type}</>,
+        enlarge: true,
+      },
+    },
+    {
+      field: "created_at",
+      name: "Created at",
+      "data-test-subj": "createdAtCell",
+      mobileOptions: {
+        render: (segment: Segment) =>
+          formatDate(moment(segment.created_at).format("YYYY-MM-DD HH:mm:ss"), "createdAt"),
+        enlarge: true,
+      },
+    },
+  ];
+
+  const getRowProps = (segment: Segment) => {
+    const { id } = segment;
+    return {
+      "data-test-subj": `row-${id}`,
+      className: "customRowClass",
+      onClick: () => {
+        router.push(`${pathPrefix}/dashboards/segments/info/${id}`);
+      },
+    };
+  };
+
+  const getCellProps = (segment: Segment, column: EuiTableFieldDataColumnType<Segment>) => {
+    const { id } = segment;
+    const { field } = column;
+
+    return {
+      className: "customCellClass",
+      "data-test-subj": `cell-${id}-${String(field)}`,
+      textOnly: true,
+    };
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <EuiBasicTable
+      tableCaption="Demo of EuiBasicTable"
+      items={data || []}
+      rowHeader="firstName"
+      columns={columns}
+      rowProps={getRowProps}
+      cellProps={getCellProps}
+    />
+  );
+};
+
+export default SegmentsTable;
--- src/pages/dashboards/segments/index.tsx
+++ src/pages/dashboards/segments/index.tsx
@@ -1,16 +1,8 @@
-import {
-  EuiBasicTable,
-  EuiBasicTableColumn,
-  EuiBreadcrumbs,
-  EuiButton,
-  EuiTableFieldDataColumnType,
-  useEuiTheme,
-} from "@elastic/eui";
-import moment from "moment";
+import { EuiBreadcrumbs, EuiButton, useEuiTheme } from "@elastic/eui";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
-import useGetSegments, { Segment } from "../../../hooks/useGetSegments";
+import SegmentsTable from "../../../components/segments/table";
 import DashboardLayout from "../../../layouts/dashboard";
 import { dashboardsStyles } from "../../../styles/dashboards.styles";
 
@@ -20,80 +12,6 @@ const Dashboard: FunctionComponent = () => {
   const { euiTheme } = useEuiTheme();
   const router = useRouter();
   const styles = dashboardsStyles(euiTheme);
-  const { data, isLoading } = useGetSegments<Segment[]>();
-  const columns: Array<EuiBasicTableColumn<Segment>> = [
-    {
-      field: "id",
-      name: "ID",
-      "data-test-subj": "idCell",
-      mobileOptions: {
-        render: (segment: Segment) => <>{segment.id}</>,
-        enlarge: true,
-      },
-    },
-    {
-      field: "name",
-      name: "Name",
-      "data-test-subj": "nameCell",
-      mobileOptions: {
-        render: (segment: Segment) => <>{segment.name}</>,
-        enlarge: true,
-      },
-    },
-    {
-      field: "description",
-      name: "Description",
-      "data-test-subj": "descriptionCell",
-      mobileOptions: {
-        render: (segment: Segment) => <>{segment.description}</>,
-        enlarge: true,
-      },
-    },
-    {
-      field: "type",
-      name: "Type",
-      "data-test-subj": "typeCell",
-      mobileOptions: {
-        render: (segment: Segment) => <>{segment.type}</>,
-        enlarge: true,
-      },
-    },
-    {
-      field: "created_at",
-      name: "Created at",
-      "data-test-subj": "createdAtCell",
-      mobileOptions: {
-        render: (segment: Segment) => moment(segment.updated_at).format("YYYY-MM-DD HH:mm:ss"),
-        enlarge: true,
-      },
-    },
-  ];
-
-  const getRowProps = (segment: Segment) => {
-    const { id } = segment;
-    return {
-      "data-test-subj": `row-${id}`,
-      className: "customRowClass",
-      onClick: () => {
-        router.push(`${pathPrefix}/dashboards/segments/info/${id}`);
-      },
-    };
-  };
-
-  const getCellProps = (segment: Segment, column: EuiTableFieldDataColumnType<Segment>) => {
-    const { id } = segment;
-    const { field } = column;
-
-    return {
-      className: "customCellClass",
-      "data-test-subj": `cell-${id}-${String(field)}`,
-      textOnly: true,
-    };
-  };
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <>
@@ -131,14 +49,7 @@ const Dashboard: FunctionComponent = () => {
         }
       >
         <div css={styles.container}>
-          <EuiBasicTable
-            tableCaption="Demo of EuiBasicTable"
-            items={data || []}
-            rowHeader="firstName"
-            columns={columns}
-            rowProps={getRowProps}
-            cellProps={getCellProps}
-          />
+          <SegmentsTable />
         </div>
       </DashboardLayout>
     </>
