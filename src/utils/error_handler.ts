098c171e07f2736971db26bf7804d9813821c979
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-06-26 11:52:30 +0800

    response bug zaswar

--- src/utils/error_handler.ts
+++ src/utils/error_handler.ts
@@ -1,4 +1,4 @@
-export async function handleResponseNotOk(res: Response) {
+export async function handleResponseNotOk(res) {
   if (!res.ok) {
     const data = await res.json();
 
@@ -25,5 +25,5 @@ export async function handleResponseNotOk(res: Response) {
     throw error;
   }
 
-  return res.json();
+  return res;
 }
