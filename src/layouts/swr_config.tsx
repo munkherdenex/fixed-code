15edde40c2936b80c52c2a408777c96cd03569b6
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-07-19 15:43:29 +0800

    401 baih uyed garah logic zaswarlasan

--- src/layouts/swr_config.tsx
+++ src/layouts/swr_config.tsx
@@ -1,22 +1,25 @@
 import Cookies from "js-cookie";
-import { useRouter } from "next/router";
 import { mutate, SWRConfig } from "swr";
 import useLogout from "../hooks/useLogout";
 
 const SWRConfigLayout = ({ children }) => {
-  const router = useRouter();
   const { trigger } = useLogout();
 
   return (
     <SWRConfig
       value={{
         onError: async (error) => {
-          if (error?.status === 401 && router.pathname.includes("dashboard")) {
+          if (error?.status === 401) {
             await trigger();
             await mutate(() => true, undefined, { revalidate: false });
             Cookies.remove("_customer_data_session", { path: "/" });
             localStorage.removeItem("currentTeamId");
             window.location.href = "/";
+          }
+        },
+        onErrorRetry: (error) => {
+          if (error?.status === 401) {
+            return;
           }
         },
       }}
