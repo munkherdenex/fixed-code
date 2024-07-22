18471f4cfc1b8619983f888ef053e87ca7a750fb
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-07-22 15:35:22 +0800

    bug fix

--- src/layouts/swr_config.tsx
+++ src/layouts/swr_config.tsx
@@ -1,15 +1,17 @@
 import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 import { mutate, SWRConfig } from "swr";
 import useLogout from "../hooks/useLogout";
 
 const SWRConfigLayout = ({ children }) => {
+  const router = useRouter();
   const { trigger } = useLogout();
 
   return (
     <SWRConfig
       value={{
         onError: async (error) => {
-          if (error?.status === 401) {
+          if (error?.status === 401 && router.pathname.includes("dashboards")) {
             await trigger();
             await mutate(() => true, undefined, { revalidate: false });
             Cookies.remove("_customer_data_session", { path: "/" });
