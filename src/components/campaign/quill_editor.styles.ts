43a5b6fd4ec6eafd0a2942685198e123113cdb97
Gendee <bgendee1@gmail.com>
2024-08-02 02:47:05 +0000

    update email quill editor

--- src/components/campaign/quill_editor.styles.ts
+++ src/components/campaign/quill_editor.styles.ts
@@ -1,5 +1,17 @@
 import { css } from "@emotion/react";
 
-export const quillEditorStyles = () => ({
-  container: css``,
+export const quillEditorStyles = (data_null=null) => ({
+  quill_container: css`
+    height: 140px;
+    max-height: 150px;
+    margin-bottom: 70px;
+  `,
+  quill_result: css`
+    max-width: 100px;
+    max-height: 200px;
+  `,
+  quill_edit: css`
+    min-width: 100%;
+    min-height: 280px;
+  `,
 });
