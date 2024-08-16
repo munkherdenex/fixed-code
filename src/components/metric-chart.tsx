34698187b211f5b6d2b2a583364ac3343fc45230
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-08-16 13:46:40 +0800

    code zaswar

--- src/components/metric-chart.tsx
+++ src/components/metric-chart.tsx
@@ -1,12 +1,5 @@
 import { Chart, DARK_THEME, LayoutDirection, LIGHT_THEME, Metric, Settings } from "@elastic/charts";
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiPanel,
-  EuiSelect,
-  useEuiTheme,
-  useGeneratedHtmlId,
-} from "@elastic/eui";
+import { EuiFlexGroup, EuiFlexItem, EuiPanel, useEuiTheme } from "@elastic/eui";
 import { useState } from "react";
 import useGetMetrics, { MetricResponse, MetricType } from "../hooks/useGetMetrics";
 import { commonStyles } from "../styles/global.styles";
@@ -19,12 +12,11 @@ const options = [
 ];
 
 const MetricChart = () => {
-  const basicSelectId = useGeneratedHtmlId({ prefix: "basicSelect" });
   const { colorMode } = useEuiTheme();
   const cStyles = commonStyles();
-  const { data } = useGetMetrics<MetricResponse>();
+  const { data, isLoading } = useGetMetrics<MetricResponse>();
 
-  const [metricDay, setMetricDay] = useState(options[1].value);
+  const [metricDay] = useState(options[1].value);
 
   const chartBaseTheme = colorMode === "DARK" ? DARK_THEME : LIGHT_THEME;
 
@@ -33,23 +25,12 @@ const MetricChart = () => {
     maximumFractionDigits: 3,
   });
 
-  const onChange = (e) => {
-    setMetricDay(e.target.value);
-  };
+  const currentData = data?.[metricDay] as MetricType;
 
-  const currentData = data[metricDay] as MetricType;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <EuiFlexGroup direction="column">
-      <EuiFlexItem>
-        <EuiSelect
-          id={basicSelectId}
-          options={options}
-          value={metricDay}
-          onChange={(e) => onChange(e)}
-          aria-label="Use aria labels when no actual label is in use"
-        />
-      </EuiFlexItem>
       <EuiFlexItem>
         <EuiFlexGroup>
           <EuiFlexItem grow={1}>
@@ -65,6 +46,7 @@ const MetricChart = () => {
                           {
                             color: colorMode === "DARK" ? "#1D1E24" : "white",
                             title: "Audience (API)",
+                            extra: <div>Last 24 hour change</div>,
                             value: currentData?.customers_created_api,
                             valueFormatter: (v) => formatter.format(v),
                           },
