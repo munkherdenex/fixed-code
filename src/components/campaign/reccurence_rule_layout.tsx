809c929a9992817cac5f9e066ec94b0902db6637
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-10-01 18:02:38 +0800

    ui changed

--- src/components/campaign/reccurence_rule_layout.tsx
+++ src/components/campaign/reccurence_rule_layout.tsx
@@ -5,6 +5,10 @@ import {
   EuiFlyout,
   EuiFlyoutBody,
   EuiFlyoutHeader,
+  EuiIcon,
+  EuiSpacer,
+  EuiStat,
+  EuiTextColor,
   EuiTitle,
 } from "@elastic/eui";
 import moment from "moment";
@@ -39,49 +43,63 @@ const ReccurenceRuleLayout = () => {
 
   return (
     <>
-      <EuiFlexGroup direction="column" gutterSize="s">
-        {data?.start_date || data?.end_date || data?.recur_count ? (
-          <EuiFlexItem>
-            <EuiFlexGroup gutterSize="xs" direction="column" justifyContent="spaceBetween">
-              {data?.start_date && (
-                <EuiFlexItem>
-                  Start date: {moment(data?.start_date).format("YYYY-MM-DD LT")}
-                </EuiFlexItem>
-              )}
-              {data?.end_date && (
-                <EuiFlexItem>
-                  End date: {moment(data?.end_date).format("YYYY-MM-DD LT")}
-                </EuiFlexItem>
-              )}
-              {data?.recur_count ? (
-                <EuiFlexItem>Recur count: {data?.recur_count}</EuiFlexItem>
-              ) : (
-                <></>
-              )}
-            </EuiFlexGroup>
-          </EuiFlexItem>
-        ) : (
-          <EuiFlexItem>No recurrence scheduled</EuiFlexItem>
-        )}
-        {data?.start_date && isDraft && (
-          <EuiFlexItem>
-            <EuiButton
-              color="primary"
-              iconType="timeRefresh"
-              onClick={() => setIsFlyoutVisible(true)}
-            >
-              Update Recurrence Rule
-            </EuiButton>
-          </EuiFlexItem>
-        )}
-        {!data?.start_date && isDraft && (
-          <EuiFlexItem>
-            <EuiButton color="primary" iconType="plus" onClick={() => setIsFlyoutVisible(true)}>
-              Add Recurrence Rule
-            </EuiButton>
-          </EuiFlexItem>
-        )}
-      </EuiFlexGroup>
+      <EuiStat
+        title={
+          <EuiFlexGroup direction="column" gutterSize="s">
+            {data?.start_date || data?.end_date || data?.recur_count ? (
+              <EuiFlexItem>
+                <EuiFlexGroup gutterSize="xs" direction="column" justifyContent="spaceBetween">
+                  {data?.start_date && (
+                    <EuiFlexItem>
+                      Start date: {moment(data?.start_date).format("YYYY-MM-DD LT")}
+                    </EuiFlexItem>
+                  )}
+                  {data?.end_date && (
+                    <EuiFlexItem>
+                      End date: {moment(data?.end_date).format("YYYY-MM-DD LT")}
+                    </EuiFlexItem>
+                  )}
+                  {data?.recur_count ? (
+                    <EuiFlexItem>Recur count: {data?.recur_count}</EuiFlexItem>
+                  ) : (
+                    <></>
+                  )}
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            ) : (
+              <EuiFlexItem>No recurrence scheduled</EuiFlexItem>
+            )}
+          </EuiFlexGroup>
+        }
+        description={
+          <EuiTextColor color="subdued">
+            <span>
+              <EuiIcon type="tokenDate" /> Reccurence
+            </span>
+          </EuiTextColor>
+        }
+        titleColor=""
+        titleSize="xs"
+      />
+      <EuiSpacer size="s" />
+      {data?.start_date && isDraft && (
+        <EuiFlexItem>
+          <EuiButton
+            color="primary"
+            iconType="timeRefresh"
+            onClick={() => setIsFlyoutVisible(true)}
+          >
+            Update Recurrence Rule
+          </EuiButton>
+        </EuiFlexItem>
+      )}
+      {!data?.start_date && isDraft && (
+        <EuiFlexItem>
+          <EuiButton color="primary" iconType="plus" onClick={() => setIsFlyoutVisible(true)}>
+            Add Recurrence Rule
+          </EuiButton>
+        </EuiFlexItem>
+      )}
       {isFlyoutVisible && isDraft && <Flyout setIsFlyoutVisible={setIsFlyoutVisible} />}
     </>
   );
