b18f387e8e09d6915b4cb3d15031be49e178d747
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-10-03 14:49:01 +0800

    improvement

--- src/components/admin_manager_component.tsx
+++ src/components/admin_manager_component.tsx
@@ -3,11 +3,11 @@ import { teamsContext } from "../store/teams_store";
 
 const AdminManagerComponent = ({ children }) => {
   const { myProfile } = useContext(teamsContext);
+  const isManager = myProfile?.role === "manager";
+  const isAdmin = myProfile?.role === "admin";
+  const isActive = myProfile?.status === "active";
 
-  if (
-    (myProfile?.role === "admin" || myProfile?.role === "manager") &&
-    myProfile?.status === "active"
-  ) {
+  if ((isAdmin || isManager) && isActive) {
     return children;
   }
 
