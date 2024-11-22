080ff97c8b6e91fa0d7e2e712f4cfe8e0349b23d
Davgatseren <davgatseren@gmail.com>
2024-11-22 16:26:35 +0800

    update

--- src/components/management/team_general_info.tsx
+++ src/components/management/team_general_info.tsx
@@ -1,15 +1,11 @@
 import {
-  EuiButton,
   EuiButtonEmpty,
   EuiButtonIcon,
   EuiFlexGroup,
   EuiFlexItem,
-  EuiListGroup,
-  EuiListGroupItem,
   EuiPanel,
   EuiSpacer,
   EuiStat,
-  EuiText,
   EuiTextColor,
   EuiTimeline,
 } from "@elastic/eui";
@@ -18,9 +14,20 @@ import { useContext, useMemo } from "react";
 import { useManagementTeamsContext } from "../../store/management_teams_store";
 import { teamsContext } from "../../store/teams_store";
 import { convertToTree } from "../../utils/convertToTree";
+import useUpdateTeamName from "../../hooks/useUpdateTeamName";
 
 const TeamGeneralInfo = () => {
+  const router = useRouter();
   const { currentTeam } = useManagementTeamsContext();
+  const { trigger } = useUpdateTeamName(router?.query?.id);
+
+  const update = () => {
+    trigger({
+      name: "Pocket-ийн баг",
+      description: "",
+      admin_id: 1,
+    });
+  };
 
   return (
     <>
@@ -28,7 +35,20 @@ const TeamGeneralInfo = () => {
         <EuiFlexGroup direction="column">
           <EuiFlexItem>
             <EuiPanel paddingSize="s" color="subdued">
-              Team info
+              <EuiFlexGroup justifyContent="spaceBetween">
+                <EuiFlexItem grow={false}>Team info</EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexItem grow={false}>
+                    <EuiButtonIcon
+                      display="base"
+                      iconType="pencil"
+                      aria-label="Update"
+                      color="primary"
+                      onClick={() => update()}
+                    />
+                  </EuiFlexItem>
+                </EuiFlexItem>
+              </EuiFlexGroup>
             </EuiPanel>
           </EuiFlexItem>
           <EuiFlexItem>
