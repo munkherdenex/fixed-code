47e510f3c02b0ac561c7ccb27bd3641ae1b11a79
Enkuushka <enkh.mon@gmail.com>
2025-07-21 11:13:34 +0800

    No team has better message and sign out button.

--- src/components/no_team.tsx
+++ src/components/no_team.tsx
@@ -1,13 +1,28 @@
-import { EuiPageTemplate, EuiEmptyPrompt } from "@elastic/eui";
+import { EuiPageTemplate, EuiEmptyPrompt, EuiButton } from "@elastic/eui";
+import { mutate } from "swr";
+import useLogout from "../hooks/useLogout";
 
 const NoTeam = () => {
+  const { trigger } = useLogout();
   return (
     <EuiPageTemplate>
       <EuiPageTemplate.EmptyPrompt>
         <EuiEmptyPrompt
-          body={<p>{"Please contact your admin"}</p>}
+          body={
+            <>
+              <p>{"Админ эрхтэй хүнтэй холбоо барина уу."}</p>
+              <EuiButton color="danger" onClick={async () => {
+                await trigger();
+                await mutate(() => true, undefined, { revalidate: false });
+                localStorage.removeItem("currentTeamId");
+                window.location.href = "/";
+              }}>
+                Гарах
+              </EuiButton>
+            </>
+          }
           layout="vertical"
-          title={<h2>No permission</h2>}
+          title={<h2>Багт ороогүй байна</h2>}
           titleSize="m"
         />
       </EuiPageTemplate.EmptyPrompt>
